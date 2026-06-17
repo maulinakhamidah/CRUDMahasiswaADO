aafb8c27d4ea7bda21c260c633299e102d934cdd
maulinakhamidah <maulinakhamidah115@gmail.com>
2026-06-17 19:28:12 +0700

    sebagai penghubung ke dalam class

--- CRUDMahasiswaADO/Form3.cs
+++ CRUDMahasiswaADO/Form3.cs
@@ -1,4 +1,52 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class Form3 : Form
+    {
+        private static string connectionString = @"Data Source=LAPTOP-66MU6CLK\MAULINAA;Initial Catalog=DBAkademikADO;Integrated Security=True";
+        private SqlConnection conn;
+        private SqlDataAdapter da;
+        private DataTable dtMahasiswa;
+        private CrystalReport1 listMahasiswa = new CrystalReport1();
+
+        string prodi { get; set; }
+        DateTime tglmasuk { get; set; }
+
+        public Form3(string Prodi, DateTime TglMasuk)
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+
+            prodi = Prodi;
+            tglmasuk = TglMasuk;
+
+            crystalReportViewer2.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
+
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                SqlCommand cmd = new SqlCommand("sp_Report", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@inProdi", prodi);
+                cmd.Parameters.AddWithValue("@inTglMsuk", tglmasuk.Year);
+
+                da = new SqlDataAdapter(cmd);
+                dtMahasiswa = new DataTable();
+                da.Fill(dtMahasiswa);
+
+                conn.Close();
 
                 listMahasiswa.SetDataSource(dtMahasiswa);
                 crystalReportViewer2.ReportSource = listMahasiswa;
@@ -10,4 +58,14 @@
             }
         }
 
-  
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void crystalReportViewer2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
