38fa192608a131ff7d0712b967aa2c0e9fecd1f0
maulinakhamidah <maulinakhamidah115@gmail.com>
2026-06-17 19:08:02 +0700

    Form mengirim input user ke DAL untuk disimpan ke SQL

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -1,31 +1,47 @@
 ﻿
 
-        private void btnUpdate_Click(object sender, EventArgs e)
+        private void btnInsert_Click(object sender, EventArgs e)
         {
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            SqlTransaction trans = conn.BeginTransaction();
             try
             {
-                using (SqlConnection conn = new SqlConnection(connectionString))
-                {
-                    using (SqlCommand cmd = new SqlCommand("sp_UpdateMahasiswa", conn))
-                    {
-                        cmd.CommandType = CommandType.StoredProcedure;
-                        cmd.Parameters.AddWithValue("@NIM", txtNIM.Text);
-                        cmd.Parameters.AddWithValue("@Nama", txtNama.Text);
-                        cmd.Parameters.AddWithValue("@JenisKelamin", cmbJK.Text);
-                        cmd.Parameters.AddWithValue("@TanggalLahir", dtpTanggalLahir.Value.Date);
-                        cmd.Parameters.AddWithValue("@Alamat", txtAlamat.Text);
+                SqlCommand cmd = new SqlCommand("sp_InsertMahasiswa", conn, trans);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@NIM", txtNIM.Text);
+                cmd.Parameters.AddWithValue("@Nama", txtNama.Text);
+                cmd.Parameters.AddWithValue("@JenisKelamin", cmbJK.Text);
+                cmd.Parameters.AddWithValue("@TanggalLahir", dtpTanggalLahir.Value.Date);
+                cmd.Parameters.AddWithValue("@Alamat", txtAlamat.Text);
+                cmd.Parameters.AddWithValue("@TanggalDaftar", DateTime.Now);
+                cmd.ExecuteNonQuery();
 
-                        conn.Open();
-                        cmd.ExecuteNonQuery();
-                    }
-                    MessageBox.Show("Data mahasiswa berhasil diubah");
-                    LoadData();
-                }
+                SqlCommand cmdLog = new SqlCommand(
+                    "INSERT INTO LogAktivitasSalah (aktivitas, waktu) VALUES (@Aktivitas, GETDATE())",
+                    conn, trans);
+                cmdLog.Parameters.AddWithValue("@aktivitas", "INSERT MAHASISWA : " + txtNIM.Text);
+                cmdLog.ExecuteNonQuery();
+
+                trans.Commit();
+                MessageBox.Show("Data berhasil ditambahkan");
+                LoadData();
+            }
+            catch (SqlException ex)
+            {
+                trans.Rollback();
+                SimpanLog("ROLLBACK INSERT MAHASISWA : " + ex.Message);
+                MessageBox.Show("Gagal menambahkan data: " + ex.Message);
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error: " + ex.Message);
+                trans.Rollback();
+                SimpanLog("GENERAL ERROR : " + ex.Message);
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
             }
         }
 
-    
