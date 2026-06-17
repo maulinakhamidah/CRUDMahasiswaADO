9ff67714923f5e16c4e49c5978545696d6856d73
maulinakhamidah <maulinakhamidah115@gmail.com>
2026-06-17 19:40:22 +0700

    menampilkan data grafik statistik mahasiswa berdasarkan tahun angkatan

--- DAL.cs
+++ DAL.cs
@@ -1,14 +1,15 @@
 ﻿
 
-        public DataTable getAllDataChart()
+        public DataTable getDataChartByTahun(DateTime thMasuk)
         {
             if (conn.State == ConnectionState.Closed) conn.Open();
-            SqlCommand cmd = new SqlCommand("sp_Dashboard", conn);
+            SqlCommand cmd = new SqlCommand("sp_DashboardByTahun", conn);
             cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@inTglMsuk", thMasuk.Year);
             da = new SqlDataAdapter(cmd);
             dtMahasiswa = new DataTable();
             da.Fill(dtMahasiswa);
             return dtMahasiswa;
         }
-
-       
+    }
+}
