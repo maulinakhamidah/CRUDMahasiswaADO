66e496f58b0f3288b28cdac3ffb86cf66dea501a
maulinakhamidah <maulinakhamidah115@gmail.com>
2026-06-17 19:16:13 +0700

    implement navigation to rekap report form

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -1,32 +1,20 @@
 ﻿
 
-        private void btnResetData_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            try
-            {
-                using (SqlConnection conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
-                    string query = @"
-                        IF OBJECT_ID('dbo.Mahasiswa_Backup') IS NOT NULL
-                        BEGIN
-                            DELETE FROM dbo.Mahasiswa;
-                            INSERT INTO dbo.Mahasiswa
-                            SELECT * FROM dbo.Mahasiswa_Backup;
-                        END";
-
-                    using (SqlCommand cmd = new SqlCommand(query, conn))
-                    {
-                        cmd.ExecuteNonQuery();
-                    }
-                }
-                MessageBox.Show("Data mahasiswa berhasil direset");
-                LoadData();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error: " + ex.Message);
-            }
+            RekapMahasiswa fm3 = new RekapMahasiswa("", DateTime.Now);
+            fm3.Show();
+            this.Hide();
         }
 
-   
+        private void dtpTanggalLahir_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
